8ba0a4509a3876ecb319e6489faa571815a7239c
Oleksandr Provolovskyi <oprovolovskyi@indppmi.com>
2025-06-29 22:00:05 +0300

    focus on addToCartButton

--- src/context/orders.ts
+++ src/context/orders.ts
@@ -24,6 +24,7 @@ export default class OrdersContext {
         }
 
         await this.landingPage.productItemByTitle(title).addToCartButton.hover();
+        await this.landingPage.productItemByTitle(title).addToCartButton.focus();
         await this.landingPage.productItemByTitle(title).addToCartButton.click();
     }
 
